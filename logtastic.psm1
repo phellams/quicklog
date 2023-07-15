3bb4b88fe4f82e03c5420bcf0c2667326b882d09
sgkens <sgkens@snowlab.tk>
2023-07-15 23:57:24 +1000

    refactor: updated module root file

--- logtastic.psm1
+++ logtastic.psm1
@@ -1,16 +1,7 @@
 using module libs\logtastic_lib.psm1
+using module libs\cmdlets\Get-LogTasticModuleInstance.psm1
+using module libs\cmdlets\New-LogTastic.psm1
 using module libs\cmdlets\Write-LogTastic.psm1
+using module libs\cmdlets\Write-LogTasticProgress.psm1
 
-$logtastic = [logtastic]::new($null)
-
-Function Get-LogTasticModuleInstance() {
-    [alias("gltmi")]
-    [CmdletBinding()]
-    [OutPutType([object])]
-    param()
-    process{
-        return $logtastic
-    }
-}
-
-Export-ModuleMember -Function Write-LogTastic, Get-LogTasticModuleInstance
+Export-ModuleMember -Function Write-LogTastic, Get-LogTasticModuleInstance, Write-LogTasticProgress
