92ad465df56185ac6d4c2dff6663f3a5f4801412
sgkens <sgkens@gmail.com>
2023-12-18 07:57:11 +1100

    🔧 chore: re-organise files

--- idea_examples/cmdlet-examples.ps1
+++ idea_examples/cmdlet-examples.ps1
@@ -1,12 +1,12 @@
 
-using module ..\Logtastic.psm1
+import-module -name .\ -force
 #! CMDLET TESTING
 
 $logname = "Logtastic"
 
 
 # Default Message with no param given
-Write-LogTastic "Default Message Type=info Message no param switch"
+Write-LogTastic "Default Message Type=info Message no param switch" -Name $logname
 Write-LogTastic -Message "Default Message Type=info with logname" -Name $logname
 
 [string[]]$types = "info","action", "success", "error", "complete"
@@ -16,17 +16,17 @@ foreach($type in $types){
   Write-LogTastic -Message "$type Message" -Name $logname -Type $type
   start-sleep -Milliseconds 500
   Write-LogTastic -Message "$type Message Custom unicode" -Name $logname -Type $type -unicode "#1F517"
-  start-sleep -Milliseconds 500
+  start-sleep -Milliseconds 8000
   Write-LogTastic -Message "$type Message with propture @{pt:{prop=propvalue}}" -Name $logname -Type $type
-  start-sleep -Milliseconds 500
+  start-sleep -Milliseconds 2000
   Write-LogTastic -Message "$type Message with colortune {ct:blue:blue colored Text}" -Name $logname -Type $type  
-  start-sleep -Milliseconds 500
+  start-sleep -Milliseconds 200
   Write-LogTastic -Message "$type SubMessage" -Name $logname -Type $type -SubMessage
-  start-sleep -Milliseconds 500
+  start-sleep -Milliseconds 100
   Write-LogTastic -Message "$type Message With No Exection Time" -Name $logname -Type $type -NoExecTime
   start-sleep -Milliseconds 500
   Write-LogTastic -Message "$type Message With No Emoji Icon" -Name $logname -Type $type -noLogIcon
-  start-sleep -Milliseconds 500
+  start-sleep -Milliseconds 3000
   Write-LogTastic -Message "$type Message With No Date/Time Stamp" -Name $logname -Type $type -noDateTime
   start-sleep -Milliseconds 500
   Write-LogTastic -Message "$type Message With No Emoji Icon and No Date/Time Stamp" -Name $logname -Type $type -NoDateTime -NoLogIcon
@@ -54,7 +54,5 @@ for($i=1;$i -lt $max; $i++){
                           -Total "369mb" `
                           -Bandwidth "467.2mb" `
                           -Transferred "863.2gb" `
-                          -Eta "8Mins, 12Secs" `
-                          -NoDateTime $NoDateTime `
-                          -NoLogIcon $NoLogIcon
+                          -Eta "8Mins, 12Secs" 
 }
