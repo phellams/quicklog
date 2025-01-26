d4d4d03a73721b228b1bb60318a511f49ad75406
sgkens <sgkens@gmail.com>
2025-01-26 14:15:42 +1100

    ✅ tests(pester): ADD: pester unit test script

--- test/Test-Unit-Pester.ps1
+++ test/Test-Unit-Pester.ps1
@@ -1,57 +1,84 @@
-using module ..\Logtastic.psm1
-
-BeforeAll {
-}
+BeforeAll { import-module .\quicklog.psm1 }
 
 Describe "cmdlets" {
-  it "Get-LogTasticModuleInstance should be type logtastic" {
-    $(Get-LogTasticModuleInstance).GetType().Name | Should -be logtastic
-  }
 
-  it "Write-LogTastic | Should return void" {
-    $logname = "Logtastic"
+  # it "Get-QuicklogModuleInstance should be type Quicklog" {
+  #   $(Get-QuicklogModuleInstance).GetType().Name | Should -be Quicklog
+  # }
 
-
+  it "Write-Quicklog | Should return void" {
+    $logname = "Ql"
     # Default Message with no param given
-    Write-LogTastic "Default Message Type=info Message no param switch"
-    Write-LogTastic -Message "Default Message Type=info with logname" -Name $logname
+    Write-Quicklog "Default Message {cs:yellow:Type=info} Message no param switch" | should -be $null
+    Write-Quicklog -Message "Default Message {cs:yellow:Type=info} with logname" -Name $logname | should -be $null
 
-    [string[]]$types = "info", "action", "success", "error", "complete"
+    [string[]]$types = "info", "action", "success", "error", "complete", "response", "request", "download", "upload", "read", "write"
 
     # #! Loop through types
     foreach ($type in $types) {
-      Write-LogTastic -Message "$type Message" -Name $logname -Type $type | should -be $null
-      Write-LogTastic -Message "$type Message Custom unicode" -Name $logname -Type $type -unicode "#1F517" | should -be $null
-      Write-LogTastic -Message "$type Message with propture @{pt:{prop=propvalue}}" -Name $logname -Type $type | should -be $null
-      Write-LogTastic -Message "$type Message with colortune {ct:blue:blue colored Text}" -Name $logname -Type $type  | should -be $null
-      Write-LogTastic -Message "$type SubMessage" -Name $logname -Type $type -SubMessage | should -be $null
-      Write-LogTastic -Message "$type Message With No Exection Time" -Name $logname -Type $type -NoExecTime | should -be $null
-      Write-LogTastic -Message "$type Message With No Emoji Icon" -Name $logname -Type $type -noLogIcon | should -be $null
-      Write-LogTastic -Message "$type Message With No Date/Time Stamp" -Name $logname -Type $type -noDateTime | should -be $null
-      Write-LogTastic -Message "$type Message With No Emoji Icon and No Date/Time Stamp" -Name $logname -Type $type -NoDateTime -NoLogIcon | should -be $null
+      start-sleep -milliseconds 1000
+      Write-Quicklog -Message "$type Message" -Name $logname -Type $type | should -be $null
+      Write-Quicklog -Message "$type Message Custom unicode" -Name $logname -Type $type -icon "default-icon" | should -be $null
+      Write-Quicklog -Message "$type Message @{pt:{prop=propvalue}} with propture @{pt:{prop=propvalue}}" -Name $logname -Type $type | should -be $null
+      Write-Quicklog -Message "$type Message with colortune {cs:blue:blue colored Text}" -Name $logname -Type $type  | should -be $null
+      Write-Quicklog -Message "$type SubMessage" -Name $logname -Type $type -SubMessage | should -be $null
+      Write-Quicklog -Message "$type Message With No Exection Time" -Name $logname -Type $type -NoExecTime | should -be $null
+      Write-Quicklog -Message "$type Message With No Emoji Icon" -Name $logname -Type $type -noLogIcon | should -be $null
+      Write-Quicklog -Message "$type Message With No Date/Time Stamp" -Name $logname -Type $type -noDateTime | should -be $null
+      Write-Quicklog -Message "$type Message With No Emoji Icon and No Date/Time Stamp" -Name $logname -Type $type -NoDateTime -NoLogIcon -NoExecTime | should -be $null
     }
   }
   
-  it "New-LogTastic should -be of Type: LogTastic" {
-    (New-LogTastic).gettype() | should -be LogTastic
+  it "New-Quicklog should -be of Type: Quicklog" {
+    (New-Quicklog).gettype().name | should -be Quicklog
   }
 
-  it "Write-LogTasticProgressc should -be of Type: $Null" {
-    Write-LogTasticProgress -barcount 100 `
+  it "Get-LogType should -be of Type: Object[]" {
+    (Get-QuicklogTypes).gettype().name | should -be Object[]
+  }
+
+  it "Write-QuicklogProgressc should -be of Type: $`Null" {
+    Write-QuicklogProgress -barcount 100 `
+                           -Percent 36 `
+                           -Name 'Quicklog' `
+                           -Status "Running" `
+                           -SubMessage `
+                           -Total "369mb" `
+                           -Bandwidth "467.2mb" `
+                           -Transferred "863.2gb" `
+                           -Eta "8Mins, 12Secs" `
+                           -NoDateTime `
+                           -NoLogIcon | should -be  $Null
+  
+    Write-QuicklogProgress  -BarCount 100 `
                             -Percent 36 `
-                            -Name 'Logtastic' `
+                            -Name 'Quicklog' `
                             -Status "Running" `
-                            -SubMessage `
                             -Total "369mb" `
                             -Bandwidth "467.2mb" `
                             -Transferred "863.2gb" `
-                            -Eta "8Mins, 12Secs" `
-                            -NoDateTime `
-                            -NoLogIcon | should -be $Null
-  
-    Write-LogTasticProgress -BarCount 100 `
-                            -Percent 36 `
-                            -Name 'Logtastic' `
+                            -Eta "8Mins, 12Secs" | should -be $Null
+    Write-QuicklogProgress  -BarCount 100 `
+                            -Percent 92 `
+                            -Name 'Quicklog' `
+                            -Status "Running" `
+                            -Total "369mb" `
+                            -Bandwidth "467.2mb" `
+                            -Transferred "863.2gb" `
+                            -Eta "8Mins, 12Secs" | should -be $Null
+
+    Write-QuicklogProgress  -BarCount 100 `
+                            -Percent 0 `
+                            -Name 'Quicklog' `
+                            -Status "Running" `
+                            -Total "369mb" `
+                            -Bandwidth "467.2mb" `
+                            -Transferred "863.2gb" `
+                            -Eta "8Mins, 12Secs" | should -be $Null
+
+    Write-QuicklogProgress  -BarCount 100 `
+                            -Percent 100 `
+                            -Name 'Quicklog' `
                             -Status "Running" `
                             -Total "369mb" `
                             -Bandwidth "467.2mb" `
